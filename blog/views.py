79b250de8afacc0757c747f03da3fa32a1185463
angelikamaciejewska <slonko1106@interia.pl>
2018-11-18 17:21:57 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +10,7 @@ def post_list(request):
 
 def choinka(request):
     return render(request, 'blog/swieta.html', {})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
